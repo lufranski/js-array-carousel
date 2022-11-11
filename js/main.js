da9b860e562bf0ee87216006c61077977ce8293d
lufranski <lufranski@gmail.com>
2022-11-11 17:13:14 +0100

    addEventListener click - remove class add class

--- js/main.js
+++ js/main.js
@@ -5,37 +5,56 @@
 
 const pics = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp'];
 const slideshow = document.getElementById('carousel');
-const nextButton = document.querySelector('.navbutton');
-const prevButton = document.querySelector('#carousel div:last-child');
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+const firstPic = document.querySelector('.pic');
 
-console.log(pics , slideshow , nextButton , prevButton);
+console.log(pics , slideshow , nextButton, prevButton);
 
 // Creare i div all'interno del file HTML tramite Js
 // Ciclo
 for (let i = 0; i < pics.length; i++) {
 
-    slideshow.innerHTML += `<div class="pic"><img src="${pics[i]}"></div>`;
-
     if (i == 0) {
 
-        const firstPic = document.querySelector('.pic');
-
-        firstPic.classList.add('active');
-
+        slideshow.innerHTML += `<div class="pic active"><img src="${pics[i]}"></div>`;
+        
+    } else {
+        slideshow.innerHTML += `<div class="pic"><img src="${pics[i]}"></div>`;
     }
 
-    nextButton.addEventListener('click' ,
+    
+    
+}
 
-         function(){
+const arrayPics = document.getElementsByClassName('pic');
 
-            firstPic.classList.remove('active');
+let activeItem = 0;
+
+console.log(arrayPics);
+
+nextButton.addEventListener('click' ,
+
+     function(){
+
+        // Condizioni
+        if(activeItem < arrayPics.length - 1) {
+            
+            // Tolgo la classe active all'elemento attuale
+            arrayPics[activeItem].classList.remove('active');
+
+            // Incremento
+            activeItem++;
+
+            // Aggiungo la classe active all'elemento successivo
+            arrayPics[activeItem].classList.add('active');
+
 
         }
 
-    );
+    }
 
-}
-
-console.log(slideshow);
+);
+// console.log(slideshow);
 
 // Ciclo for per far sì che al click le immagini cambino classe e quindi display state
