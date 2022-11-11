97a73f28b73b8fa5dd56882aa9f274e26e3c8709
lufranski <lufranski@gmail.com>
2022-11-11 15:30:53 +0100

    For Cicle - Step 1

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
 // Array Carousel
 
 // Variables
-const pics = [];
+// Creare un array di immagini
+
+const pics = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp'];
 const slideshow = document.getElementById('carousel');
 
 console.log(pics , slideshow);
 
-// Creare un array di immagini
-pics.push('img/01.webp');
-pics.push('img/02.webp');
-pics.push('img/03.webp');
-pics.push('img/04.webp');
-pics.push('img/05.webp');
-
-console.log(pics);
-
 // Creare i div all'interno del file HTML tramite Js
 // Ciclo
-for (i = 0; i < 5; i++) {
+for (let i = 0; i < pics.length; i++) {
 
-    slideshow.innerHTML = `<div class="pic"><img></div>`;
+    slideshow.innerHTML += `<div class="pic"><img src="${pics[i]}"></div>`;
 
-    
+    if (i == 0) {
+
+        const firstPic = document.querySelector('.pic');
+
+        firstPic.classList.add('active');
+
+    }
 
 }
 
