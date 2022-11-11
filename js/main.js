63fcc7ba0ace931e157c5c9d2d48c96dbf28c3f1
lufranski <lufranski@gmail.com>
2022-11-11 16:24:27 +0100

    addEventListener click - remove class

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@
 
 const pics = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp'];
 const slideshow = document.getElementById('carousel');
+const nextButton = document.querySelector('.navbutton');
+const prevButton = document.querySelector('#carousel div:last-child');
 
-console.log(pics , slideshow);
+console.log(pics , slideshow , nextButton , prevButton);
 
 // Creare i div all'interno del file HTML tramite Js
 // Ciclo
@@ -21,6 +23,16 @@ for (let i = 0; i < pics.length; i++) {
         firstPic.classList.add('active');
 
     }
+
+    nextButton.addEventListener('click' ,
+
+         function(){
+
+            firstPic.classList.remove('active');
+
+        }
+
+    );
 
 }
 
