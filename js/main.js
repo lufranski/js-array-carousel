f3fdbb6217157bad163b6d810e5792c75726449b
lufranski <lufranski@gmail.com>
2022-11-13 09:47:25 +0100

    addEventListener Fixed

--- js/main.js
+++ js/main.js
@@ -5,42 +5,44 @@
 
 const pics = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp'];
 const slideshow = document.getElementById('carousel');
-const nextButton = document.getElementById('next');
-const prevButton = document.getElementById('prev');
 
-console.log(pics , slideshow , nextButton, prevButton);
+console.log(pics , slideshow);
 
 // Creare i div all'interno del file HTML tramite Js
 // Ciclo
 for (let i = 0; i < pics.length; i++) {
 
     if (i == 0) {
-
+        
         slideshow.innerHTML += `<div class="pic active"><img src="${pics[i]}"></div>`;
         
     } else {
         slideshow.innerHTML += `<div class="pic"><img src="${pics[i]}"></div>`;
     }
-
-    
     
 }
+
+console.log(pics , slideshow);
 
 const arrayPics = document.getElementsByClassName('pic');
 
 let activeItem = 0;
 
-// console.log(arrayPics , activeItem);
+
+const nextButton = document.getElementById('next');
+const prevButton = document.getElementById('prev');
+
+console.log(arrayPics, nextButton, prevButton);
 
 nextButton.addEventListener('click' ,
     
     function(){
+        
         // Condizioni
         if(activeItem < arrayPics.length - 1) {
-            
+
                 // Tolgo la classe active all'elemento attuale
                 arrayPics[activeItem].classList.remove('active');
-
 
                 // Incremento
                 activeItem++;
@@ -52,6 +54,15 @@ nextButton.addEventListener('click' ,
                 if (activeItem === arrayPics.length - 1) {
 
                     nextButton.classList.add('hidden');
+                    
+                } if (activeItem == 0) {
+
+                    prevButton.classList.add('hidden');
+
+                } else if (activeItem > 0) {
+
+                    prevButton.classList.remove('hidden');
+
                 }
 
         }
@@ -59,4 +70,35 @@ nextButton.addEventListener('click' ,
     }
 
 );
+
+prevButton.addEventListener('click' , 
+    
+    function(){
+
+        if (activeItem > 0) {
+
+            prevButton.classList.remove('hidden');
+
+            arrayPics[activeItem].classList.remove('active');
+
+            // Decremento
+            activeItem--;
+
+            arrayPics[activeItem].classList.add('active');
+
+            if (activeItem == 0) {
+
+                prevButton.classList.add('hidden');
+
+            } if (activeItem < arrayPics.length - 1) {
+
+                nextButton.classList.remove('hidden');
+
+            }
+        }
+
+    }
+
+);
+
 // console.log(activeItem);
